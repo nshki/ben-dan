2a332c715bbde23e884af7307a78eb3437d3ac10
Nishiki Liu <nshki@users.noreply.github.com>
2020-01-01 12:49:05 -0800

    Adjust ScoreCalculatorTest

--- test/services/score_calculator_test.rb
+++ test/services/score_calculator_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class ScoreCalculatorTest < ActiveSupport::TestCase
-  test 'correctly calculates scores for one player' do
+  test 'correctly calculates score' do
     user1 = FactoryBot.create(:user, username: 'User 1')
     user2 = FactoryBot.create(:user, username: 'User 2')
     FactoryBot.create(:word, spelling: 'aa')
@@ -14,16 +14,20 @@ class ScoreCalculatorTest < ActiveSupport::TestCase
         [
           { player: game.player(user1).id, tile: 'a', rule: nil },
           { player: game.player(user1).id, tile: 'a', rule: nil }
+        ],
+        [
+          { player: game.player(user2).id, tile: 'a', rule: nil },
+          nil
         ]
       ]
-    placements = [{ col: 0, row: 1, tile: 0 }]
+    placements = [{ col: 0, row: 0, tile: 0 }]
 
     ScoreCalculator.call \
       game: game,
       placements: placements,
       player: game.player(user1)
 
-    assert_equal(2, game.player(user1).score)
+    assert_equal(4, game.player(user1).score)
     assert_equal(0, game.player(user2).score)
   end
 end
