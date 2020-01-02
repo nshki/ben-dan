aaa2a8024132ebc645194726ae393824f68dc295
Nishiki Liu <nshki@users.noreply.github.com>
2020-01-01 20:57:11 -0800

    Populate tiles on game creation

--- app/services/game_creator.rb
+++ app/services/game_creator.rb
@@ -2,54 +2,85 @@
 
 # Service that handles starting new games between players.
 class GameCreator
-  # Instantiates new Game instance.
-  #
-  # @param {Integer} num_horiz_tiles - Number of horizontal tiles (cols)
-  # @param {Integer} num_vert_tiles - Number of vertical tiles (rows)
-  # @param {Array<User>} users - Players for this game
-  # @return {Game} - New Game instance
-  def self.call(num_horiz_tiles: 15, num_vert_tiles: 15, users: [])
-    board = generate_board \
-      num_horiz_tiles: num_horiz_tiles,
-      num_vert_tiles: num_vert_tiles
-    game = Game.create \
-      board: board,
-      users: users,
-      current_turn_user: users.sample,
-      tile_bag: generate_tile_bag(tile_count: 100)
-    deal_tiles(game: game, tile_count: 8)
+  START_COORDS = [[7, 7]].freeze
+  DL_COORDS =
+    [
+      [1, 2], [1, 12], [2, 1], [2, 4], [2, 10], [2, 13],
+      [4, 2], [4, 6], [4, 8], [4, 12], [6, 4], [6, 10],
+      [8, 4], [8, 10], [10, 2], [10, 6], [10, 8], [10, 12],
+      [12, 1], [12, 4], [12, 10], [12, 13], [13, 2], [13, 12]
+    ].freeze
+  TL_COORDS =
+    [
+      [0, 6], [0, 8], [3, 3], [3, 11], [5, 5], [5, 9], [6, 0], [6, 14],
+      [8, 0], [8, 14], [9, 5], [9, 9], [11, 3], [11, 11], [14, 6], [14, 8]
+    ].freeze
+  DW_COORDS =
+    [
+      [1, 5], [1, 9], [3, 7], [5, 1], [5, 13], [7, 3], [7, 11],
+      [9, 1], [9, 13], [11, 9], [11, 7], [13, 5], [13, 9]
+    ].freeze
+  TW_COORDS =
+    [
+      [0, 3], [0, 11], [3, 0], [3, 14], [11, 0], [11, 14], [14, 3], [14, 11]
+    ].freeze
 
-    game
-  end
+  class << self
+    # Instantiates new Game instance.
+    #
+    # @param {Array<User>} users - Players for this game
+    # @return {Game} - New Game instance
+    def call(users: [])
+      @board = Array.new(15) { Array.new(15) }
+      populate_tiles
+      @game = Game.create \
+        board: @board,
+        users: users,
+        current_turn_user: users.sample,
+        tile_bag: generate_tile_bag(tile_count: 100)
+      deal_hands(tile_count: 8)
 
-  # Given dimensions, generates a new two-dimensional array.
-  #
-  # @param {Integer} num_horiz_tiles - Number of horizontal tiles (cols)
-  # @param {Integer} num_vert_tiles - Number of vertical tiles (rows)
-  # @return {Array<Array>} - Two-dimensional array
-  def self.generate_board(num_horiz_tiles:, num_vert_tiles:)
-    Array.new(num_horiz_tiles) { Array.new(num_vert_tiles) }
-  end
+      @game
+    end
 
-  # Given a tile count, generates a randomized array of tiles.
-  #
-  # @param {Integer} tile_count - Number of tiles to put in bag
-  # @return {Array<String>} - Randomized array of tiles
-  def self.generate_tile_bag(tile_count:)
-    tile_bag = []
-    tile_count.times { tile_bag.push(Game::TILES.sample) }
-    tile_bag
-  end
+    private
 
-  # Given a Game, deal hands to each player.
-  #
-  # @param {Game} game - A game
-  # @param {Integer} tile_count - Number of tiles to deal to each player
-  # @return {void}
-  def self.deal_tiles(game:, tile_count:)
-    game.users.each do |user|
-      player = game.player(user)
-      player.update(hand: game.pull_random_tiles(count: tile_count))
+    # Fill in tiles into the newly generated board.
+    #
+    # @return {void}
+    def populate_tiles
+      fill = proc do |coords, rule|
+        coords.each do |col, row|
+          @board[col][row] = { tile: nil, player: nil, rule: rule }
+        end
+      end
+
+      fill.call(DL_COORDS, :dl)
+      fill.call(TL_COORDS, :tl)
+      fill.call(DW_COORDS, :dw)
+      fill.call(TW_COORDS, :tw)
+      fill.call(START_COORDS, :start)
+    end
+
+    # Given a tile count, generates a randomized array of tiles.
+    #
+    # @param {Integer} tile_count - Number of tiles to put in bag
+    # @return {Array<String>} - Randomized array of tiles
+    def generate_tile_bag(tile_count:)
+      tile_bag = []
+      tile_count.times { tile_bag.push(Game::TILES.sample) }
+      tile_bag
+    end
+
+    # Deal hands to each player.
+    #
+    # @param {Integer} tile_count - Number of tiles to deal to each player
+    # @return {void}
+    def deal_hands(tile_count:)
+      @game.users.each do |user|
+        player = @game.player(user)
+        player.update(hand: @game.pull_random_tiles(count: tile_count))
+      end
     end
   end
 end
--- test/services/game_creator_test.rb
+++ test/services/game_creator_test.rb
@@ -6,19 +6,16 @@ class GameCreatorTest < ActiveSupport::TestCase
   test 'creates a new game given board dimensions and users' do
     user1 = FactoryBot.create(:user, username: 'User 1')
     user2 = FactoryBot.create(:user, username: 'User 2')
-    result = GameCreator.call \
-      num_horiz_tiles: 2,
-      num_vert_tiles: 2,
-      users: [user1, user2]
+    game = GameCreator.call(users: [user1, user2])
 
-    assert(result.is_a?(Game))
-    assert_equal(2, result.board.length)
-    assert_equal(2, result.board.first.length)
-    assert(result.users.include?(user1))
-    assert(result.users.include?(user2))
-    assert(result.current_turn_user.in?([user1, user2]))
-    assert_equal(84, result.tile_bag.count)
-    assert_equal(8, result.game_users.find_by(user: user1).hand.count)
-    assert_equal(8, result.game_users.find_by(user: user2).hand.count)
+    assert(game.is_a?(Game))
+    assert_equal(15, game.board.length)
+    assert_equal(15, game.board.first.length)
+    assert(game.users.include?(user1))
+    assert(game.users.include?(user2))
+    assert(game.current_turn_user.in?([user1, user2]))
+    assert_equal(84, game.tile_bag.count)
+    assert_equal(8, game.player(user1).hand.count)
+    assert_equal(8, game.player(user2).hand.count)
   end
 end
