c9d5d50da69d6e82271a6c0ffa943cbe513c067b
Nishiki Liu <nshki@users.noreply.github.com>
2020-01-05 21:05:27 -0800

    Get tile rules to render

---
+++ app/presenters/tile_presenter.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# Given a tile, provides methods to assist in rendering it.
+class TilePresenter
+  # Constructor.
+  #
+  # @param {Hash} tile - { tile: String, rule: String }
+  # @param {String} bem_base - Base class to extend with BEM
+  # @return {TilePresenter} - Instance
+  def initialize(tile:, bem_base:)
+    @tile = tile&.symbolize_keys
+    @bem_base = bem_base
+  end
+
+  # Placed tile, if any.
+  #
+  # @return {String} - Tile content
+  def content
+    return if @tile.blank?
+
+    @tile[:tile]
+  end
+
+  # The value to be plugged directly into a DOM element's `class` attribute.
+  #
+  # @return {String} - `class` attribute value
+  def css_classes
+    classes = [@bem_base]
+    classes.push("#{@bem_base}--#{@tile[:rule]}") if @tile.present?
+    classes.join(' ')
+  end
+end
