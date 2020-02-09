499756be2e31d8fa3d1909cf6daca1eef1e5c6eb
Nishiki Liu <nshki@users.noreply.github.com>
2020-02-08 19:08:32 -0800

    Make sure subscriptions are removed when needed

--- app/javascript/controllers/live_reload_controller.js
+++ app/javascript/controllers/live_reload_controller.js
@@ -2,13 +2,29 @@ import { Controller } from 'stimulus';
 import { createConsumer } from '@rails/actioncable';
 
 export default class extends Controller {
+  /**
+   * Subscribes to the GamesChannel on mount.
+   *
+   * @return {void}
+   */
   connect() {
     const channel = { channel: 'GamesChannel', id: this.data.get('game-id') };
     this.consumer = this.consumer || createConsumer();
-    this.consumer.subscriptions.create(channel, {
+    this.subscription = this.consumer.subscriptions.create(channel, {
       received(data) {
         Turbolinks.visit(location.toString());
       }
     });
+  }
+
+  /**
+   * Unsubscribes from the GamesChannel on dismount.
+   *
+   * @return {void}
+   */
+  disconnect() {
+    if (this.subscription) {
+      this.consumer.subscriptions.remove(this.subscription);
+    }
   }
 };
