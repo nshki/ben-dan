67475a7a82117070b71bac771cc87d1e2d5260a6
Nishiki Liu <nshki@users.noreply.github.com>
2020-02-08 16:56:24 -0800

    Use encrypted cookies for auth (#3)
    * Adjust session tests
    
    * Use encrypted cookies and locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   #
   # @return {User} - Currently logged in User or nil
   def current_user
-    User.find_by(id: session[:user_id])
+    User.find_by(id: cookies.encrypted[:user_id])
   end
 
   # Denotes if a User is currently signed in.
